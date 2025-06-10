197e514c9f46772468a21f2933e6f6dfcbfe4fea
janderfariasribeiro <janderfarias@hotmail.com>
2025-06-10 20:18:14 -0300

    Array e conceitos

---
+++ array.js
@@ -0,0 +1,40 @@
+//1. Criar, filtrar elementos, percorrer os itens, 
+let nomes = ['Ana', 'Bia', 'Carlos', 'Daniel', 'Eduardo']
+let carros = []
+
+let idades = [12, '25', '24']
+
+//lenght retorna o numero com a quantidade de itens da lista
+// console.log(`Tamanho: ${nomes.length}`);
+
+//2. limpar
+// console.log(idades)
+
+// idades = []
+// console.log(idades)
+
+//3. adicionar elementos
+// console.log(carro);
+
+//push adiciona elemtenos no final
+// carros.push("Fusca");
+// console.log(carros);
+
+//4. remover elementos
+//pop: remove o ulitmo elemento
+//shift: remove o primeiro elemento
+/* console.log(nomes);
+nomes.pop();
+console.log(nomes);
+nomes.shift();
+console.log(nomes);
+console.log(nomes[1])
+let nomeProprio = (`${nomes[1]} ${nomes[2]}` 
+ console.log(nomeProprio.toUpperCase())    */
+//console.log(`${nomes[1]} ${nomes[2]}`)
+
+// 6. percorrer os itens
+let frutas = ['Caju', 'Manga', 'Abacaxi', 'Maracuja', 'Goiaba','Uva']
+frutas.forEach(function(fruta){
+    console.log(fruta)
+})
