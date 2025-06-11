eda0733c9cb261b992e6af7a069d7c9b3fa12d3d
janderfariasribeiro <janderfarias@hotmail.com>
2025-06-10 21:30:09 -0300

    Praticando Objetos

---
+++ meuobjeto.js
@@ -0,0 +1,12 @@
+let curso = {
+nome:'FullStack',
+duracao:'12',
+codigoturma: 'fs25',
+turno:'noite',
+professor: 'Angelo',
+quantidadecadeiras: 20,
+projetor: true,
+nomesala: 'Grace Hopper',
+}
+//imprimindo no console
+console.log(curso)
